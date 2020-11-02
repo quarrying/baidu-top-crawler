1570aaaf65b9c3d4a5308a80e0dd38644bbb077e
quarrying <quarrying@qq.com>
2020-11-03 02:03:03 +0800

    add crawler_names

--- get_info.py
+++ get_info.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from collections import OrderedDict
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,10 +16,9 @@ def escape_records(records):
     return dst_records
 
     
-def save_markdown(filename, records, header, title):
+def save_markdown(filename, records, header):
     records = escape_records(records)
-    dst_records = ['## {}'.format(title)]
-    dst_records.append('|'.join(header))
+    dst_records = ['|'.join(header)]
     dst_records.append('|'.join(['---']*len(header)))
     for record in records:
         dst_records.append('|'.join(record))
@@ -27,8 +27,8 @@ def save_markdown(filename, records, header, title):
             f.write(str(item) + '\n')
 
 
-def crawler():
-    response = requests.get('http://top.baidu.com/buzz?b=1')
+def crawler(buzz_no=1):
+    response = requests.get('http://top.baidu.com/buzz?b={}'.format(buzz_no))
     response.encoding = 'gb18030'
     soup = BeautifulSoup(response.text, 'html.parser')
     table_tag = soup.find('table', {'class': 'list-table'})
@@ -44,11 +44,33 @@ def crawler():
     return zip(keywords, search_indices)
 
 
+def crawler_names():
+    category_buzz_no = OrderedDict([
+        ('热点人物',    258), 
+        ('名家人物',    260), 
+        ('公益人物',    612), 
+        ('财经人物',    261), 
+        ('体坛人物',    255), 
+        ('主持人',      454), 
+        ('历史人物',    259), 
+        ('互联网人物',  257), 
+        ('女明星',      1570), 
+        ('男明星',      1569),
+        ('欧美明星',    491),
+    ])
+    date_str = datetime.now().strftime('%Y%m%d')
+    for category, buzz_no in category_buzz_no.items():
+        records = crawler(buzz_no=buzz_no)
+        filename = '{} 人物之{}.md'.format(date_str, category)
+        save_markdown(filename, records, header=['关键词', '搜索指数'])
+
+
 if __name__ == '__main__':
     date_str = datetime.now().strftime('%Y%m%d')
-    filename = '{}.md'.format(date_str)
+    filename = '{} 今日热点.md'.format(date_str)
 
     records = crawler()
-    save_markdown(filename, records, header=['标题', '搜索指数'], title=date_str)
+    save_markdown(filename, records, header=['标题', '搜索指数'])
+    crawler_names()
 
 
